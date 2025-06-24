ac6b73784f115a013b57bab1b1141aeddd2c58de
Olumide Adekunle <olumideadekunle1999@yahoo.com>
2025-06-24 12:55:39 +0100

    Updated file

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ echo "Connected. Server time: " . $stmt->fetchColumn() . "<br>";
 die();
 
 
-$accountOne = new SavingsAccount("0114116070", "GTB", 100000, 0.05);
+$accountOne = new SavingsAccount("0114116970", "GTB", 100000, 0.05);
 $accountTwo = new CurrentAccount("0229399139", "Access Bank", 5000, 2000);
 
 $accountOne->deposit(2000);
