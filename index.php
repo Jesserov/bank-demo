dbdd4139be77b1ff2f92cd157f59ac79e5c3f63b
Olumide Adekunle <olumideadekunle1999@yahoo.com>
2025-06-24 18:29:11 +0100

    Added User,php, established a connection to the users table on PHPMyAdmin, and changed the ouput of of index.php

--- index.php
+++ index.php
@@ -4,14 +4,22 @@ require_once 'autoload.php';
 
 use App\Briqsbank\SavingsAccount;
 use App\Briqsbank\CurrentAccount;
+use App\Briqsbank\BankAccount;
+use App\Briqsbank\User;
 use App\Briqsbank\Database\DB;
 
-$db = DB::getInstance()->getConnection();
-$stmt = $db->query("SELECT NOW()");
-echo "Connected. Server time: " . $stmt->fetchColumn() . "<br>";
+// $db = DB::getInstance()->getConnection();
+// $stmt = $db->query("SELECT NOW()");
+// echo "Connected. Server time: " . $stmt->fetchColumn() . "<br>";
+
+// die();
+
+$user = new User();
+$user->register("Ore Makinde", "ore@gmail.com");
+
+echo ("User successfully registered.");
 
 die();
-
 
 $accountOne = new SavingsAccount("0114116970", "GTB", 100000, 0.05);
 $accountTwo = new CurrentAccount("0229399139", "Access Bank", 5000, 2000);
