740dde66ba12fdec568ffa1675be34cae4d7ee62
Olumide Adekunle <olumideadekunle1999@yahoo.com>
2025-06-24 12:33:24 +0100

    Initial commit

--- index.php
+++ index.php
@@ -4,6 +4,14 @@ require_once 'autoload.php';
 
 use App\Briqsbank\SavingsAccount;
 use App\Briqsbank\CurrentAccount;
+use App\Briqsbank\Database\DB;
+
+$db = DB::getInstance()->getConnection();
+$stmt = $db->query("SELECT NOW()");
+echo "Connected. Server time: " . $stmt->fetchColumn() . "<br>";
+
+die();
+
 
 $accountOne = new SavingsAccount("0114116070", "GTB", 100000, 0.05);
 $accountTwo = new CurrentAccount("0229399139", "Access Bank", 5000, 2000);
